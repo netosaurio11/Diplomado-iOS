a9f125adb1796634e5e48dddb128c3211fff0187
Ernesto Daniel Mejia Valdiviezo <netosaurio11@MacBook-Air-de-Ernesto.local>
2018-09-09 23:40:30 -0500

    Proyecto de Restaurant

---
+++ Entregables/Restaurant/Restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Ernesto Daniel Mejia Valdiviezo on 09/09/18.
+//  Copyright © 2018 Ernesto Daniel Mejia Valdiviezo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
