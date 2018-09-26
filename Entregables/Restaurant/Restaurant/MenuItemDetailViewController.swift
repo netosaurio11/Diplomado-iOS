d37f260c7569abe85d365ccd620c1ff53c5f232a
Ernesto Daniel Mejia Valdiviezo <netosaurio11@MacBook-Air-de-Ernesto.local>
2018-09-25 23:38:33 -0500

    Se corrigenla vist de los detalles

--- Entregables/Restaurant/Restaurant/MenuItemDetailViewController.swift
+++ Entregables/Restaurant/Restaurant/MenuItemDetailViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class MenuItemDetailViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
     var menuItem: MenuItem!
-
+    
+    func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        descriptionLabel.text = menuItem.description
+        addToOrderButton.layer.cornerRadius = 5.0
+    }
+    
+    @IBAction func addToOrderButtonTapped(_ sender: Any) {
+        UIView.animate(withDuration: 0.3) {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
     override func didReceiveMemoryWarning() {
